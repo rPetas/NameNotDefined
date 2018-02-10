a93b34efbc1e0292b9e70712d716f8cfc805b714
GuiRocha <servidor.gui@gmail.com>
2018-02-10 19:54:19 +0000

    working in the position

--- src/org/academiadecodigo/hexallents/lusozuca/Position.java
+++ src/org/academiadecodigo/hexallents/lusozuca/Position.java
@@ -1,5 +1,13 @@
 package org.academiadecodigo.hexallents.lusozuca;
 
-public class Position
-{
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+
+public class Position {
+
+    public Position(int col, int row, Stage stage){
+        this.col=col;
+        this.row=row;
+        this.stage = stage;
+        rectangle = new Rectangle(stage.)
+    }
 }
--- src/org/academiadecodigo/hexallents/lusozuca/Stage.java
+++ src/org/academiadecodigo/hexallents/lusozuca/Stage.java
@@ -1,13 +1,58 @@
 package org.academiadecodigo.hexallents.lusozuca;
 
+import javafx.geometry.Pos;
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+
 public class Stage {
 
+    public static final int PADDING =10;
+    public static final int CELL_SIZE=5;
+    private Rectangle rectangle;
     private int column;
     private int row;
+
 
     public Stage(int cols, int rows){
         column = cols;
         row = rows;
 
     }
+
+    public void init(){
+        rectangle = new Rectangle(PADDING, PADDING, getWidth(), getHeight());
+        rectangle.draw();
+    }
+
+    public int getColumn(){
+        return column;
+    }
+
+    public int getRow(){
+        return row;
+    }
+
+    public int getWidth(){
+        return column*CELL_SIZE;
+    }
+
+    public int getHeight(){
+        return row*CELL_SIZE;
+    }
+
+    public int getX(){
+        return rectangle.getX();
+    }
+
+    public int getY(){
+        return rectangle.getY();
+    }
+
+    public int getCellSize(){
+        return CELL_SIZE;
+    }
+
+    public Position makePosition(int col, int row){
+        return new Position(col, row, this);
+    }
+
 }
