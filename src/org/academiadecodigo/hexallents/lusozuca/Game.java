79bf2a0ed147c96d98acfd9ebc9f3868d97dc328
Joao Costa <jpdsdc@gmail.com>
2018-02-17 17:01:03 +0000

    more files joaocosta

--- src/org/academiadecodigo/hexallents/lusozuca/Game.java
+++ src/org/academiadecodigo/hexallents/lusozuca/Game.java
@@ -27,17 +27,20 @@ public class Game {
     public void init() {
         stage.init();
 
-        gameObjects = new GameObject[8];
-
+        gameObjects = new GameObject[10];
+        //Platform
         gameObjects[0] = new Platform(0, stage.getRow() - Platform.BLOCK_SIZE, stage, 26);    //stage.getRow() - Platform.BLOCK_SIZE = quantidade de linhas do Stage - tamanho do bloco em linhas (não em pixels)
         gameObjects[1] = new Platform(14 * Platform.BLOCK_SIZE, 15 * Platform.BLOCK_SIZE, stage, 26);
         gameObjects[2] = new Platform(0, 11 * Platform.BLOCK_SIZE, stage, 26);
         gameObjects[3] = new Platform(14 * Platform.BLOCK_SIZE, 7 * Platform.BLOCK_SIZE, stage, 26);
         gameObjects[4] = new Platform(0, 3 * Platform.BLOCK_SIZE, stage, 26);
 
+        //Stairs
         gameObjects[5] = new Stair(23 * Platform.BLOCK_SIZE, 15 * Platform.BLOCK_SIZE, stage, 4);
         gameObjects[6] = new Stair(16 * Platform.BLOCK_SIZE, 11 * Platform.BLOCK_SIZE, stage, 4);
-        gameObjects[7] = new Stair(23 * Platform.BLOCK_SIZE, 0, stage, 4);
+        gameObjects[7] = new Stair(23 * Platform.BLOCK_SIZE, 7 * Platform.BLOCK_SIZE, stage, 4);
+        gameObjects[8] = new Stair(16 * Platform.BLOCK_SIZE, 3 * Platform.BLOCK_SIZE, stage, 4);
+        gameObjects[9] = new Stair(23 * Platform.BLOCK_SIZE, -1 * Platform.BLOCK_SIZE, stage, 4);
 
 
         //Criação das plataformas: imagens
@@ -50,12 +53,11 @@ public class Game {
         Picture picPlatform4 = new Picture(stage.colToPixel(6 * Platform.BLOCK_SIZE), stage.rowToPixel(0), "/Users/codecadet/Desktop/java/NameNotDefined/src/org/academiadecodigo/hexallents/lusozuca/images/platform.png");
         picPlatform4.draw();*/
 
-        player= new Player(stage.makePosition(10,5*Platform.BLOCK_SIZE));
+        player = new Player(stage.makePosition(10, 5 * Platform.BLOCK_SIZE));
 
         collisionDetector = new CollisionDetector(gameObjects, player);
 
         player.setCollisionDetector(collisionDetector);
-
 
 
     }
